ed2f41dc8879c06131cb3e6255d1907f1486d03a
Dev Jared <ikarujerad@gmail.com>
2025-05-07 19:17:08 +0300

    added color to heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <div className="text-4xl font-bold">Hello Ateker</div>;
+  return <div className="text-4xl font-bold text-amber-100">Hello Ateker Turkana</div>;
 };
 
 export default HomePage;
