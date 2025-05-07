2a409d3ce45499f9cb646559fff95dd3878925ce
Dev Jared <ikarujerad@gmail.com>
2025-05-07 21:09:13 +0300

    setup prismaPostgres database

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 const HomePage = () => {
-  return <div className="text-4xl font-bold text-amber-100">Hello Ateker Turkana</div>;
+  return <div className="text-4xl font-bold text-amber-100">
+    Hello Ateker Turkana
+    </div>;
 };
 
 export default HomePage;
