0da0888eb7890ce89ee22e3c59d0a2c832f81864
Dev Jared <ikarujerad@gmail.com>
2025-05-07 18:46:44 +0300

    initial commit ii

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <div className="text-2xl text-red-400 font-bold">Welcome Ateker</div>;
+  return <div className="text-4xl font-bold">Hello Ateker</div>;
 };
 
 export default HomePage;
